a6c7e7ca909b769053f08fc60de2dd7f0cd634c1
Jeremy Banka <hello@jeremybanka.com>
2024-09-08 16:07:33 -0700

    🔨 add permissions update to script

--- scripts/self-sign-cert.sh
+++ scripts/self-sign-cert.sh
@@ -3,3 +3,5 @@
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
   -keyout /etc/ssl/private/selfsigned.key \
   -out /etc/ssl/certs/selfsigned.crt
+
+sudo chmod 600 /etc/ssl/private/selfsigned.key
