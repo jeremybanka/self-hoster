1f3305953d709d81f28fbee8db50b6e82bc82559
Jeremy Banka <hello@jeremybanka.com>
2024-09-09 01:45:26 -0700

    🔧 change example port

--- example.bun.ts
+++ example.bun.ts
@@ -1,20 +1,20 @@
 #!/usr/bin/env bun
 
-import { ParentSocket } from "atom.io/realtime-server"
-import { serve } from "bun"
+import { ParentSocket } from "atom.io/realtime-server";
+import { serve } from "bun";
 
-const PORT = 4444
-const parentSocket = new ParentSocket()
+const PORT = 3333;
+const parentSocket = new ParentSocket();
 
 serve({
 	port: PORT,
 	fetch(req) {
-		parentSocket.logger.info("🚀", req.method, req.url)
-		return new Response("Hello World!")
+		parentSocket.logger.info("🚀", req.method, req.url);
+		return new Response("Hello World!");
 	},
-})
+});
 
-parentSocket.emit("alive")
+parentSocket.emit("alive");
 parentSocket.on("updatesReady", () => {
-	parentSocket.emit("readyToUpdate")
-})
+	parentSocket.emit("readyToUpdate");
+});
