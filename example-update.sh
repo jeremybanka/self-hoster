1855b5989a3a1980052b8f5d7e8eb1ab78a5ef5c
Jeremy Banka <hello@jeremybanka.com>
2024-09-16 00:15:28 -0700

    🛸 use new flightdeck conventions

--- example-update.sh
+++ example-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bun build ./example.bun.ts --bundle --minify --outfile services/update/some-repo/example
+bun build ./example.bun.ts --bundle --minify --outfile services/tempest.games/update/example
