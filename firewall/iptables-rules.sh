0a9f4db7b818cbce8d2602e69a65ce03035c6d9e
Moira Banka <moirabanka@gmail.com>
2024-09-16 21:00:22 -0700

    Set default non-permissive firewall rules

--- firewall/iptables-rules.sh
+++ firewall/iptables-rules.sh
@@ -1,8 +1,17 @@
 iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -s 10.0.0.0/24 --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp -d 10.0.0.0/24 --dport 22 -j ACCEPT
+iptables -A INPUT --dport 22 -j DROP
+iptables -A OUTPUT --sport 22 -j DROP
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+
+iptables -N port-scanning
+iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
+iptables -A port-scanning -j DROP
 
 iptables --policy INPUT DROP
 iptables --policy OUTPUT DROP
