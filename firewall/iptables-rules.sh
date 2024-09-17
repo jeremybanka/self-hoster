b9bdc115f36cad158509a3547eb3600c382efbf5
Moira Banka <moirabanka@gmail.com>
2024-09-16 21:36:36 -0700

    fixes outgoing web connections

--- firewall/iptables-rules.sh
+++ firewall/iptables-rules.sh
@@ -1,8 +1,8 @@
 iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
-iptables -A INPUT --dport 22 -j DROP
-iptables -A OUTPUT --sport 22 -j DROP
+iptables -A INPUT -p tcp --dport 22 -j DROP
+iptables -A OUTPUT -p tcp --sport 22 -j DROP
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -i lo -j ACCEPT
