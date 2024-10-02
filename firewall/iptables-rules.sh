4ba3d8209e968ab9aaa9d2ad9650b0e0af8d0522
Moira Banka <moirabanka@gmail.com>
2024-10-01 21:14:34 -0700

    updated iptables rules to allow DNS, SSH, and git

--- firewall/iptables-rules.sh
+++ firewall/iptables-rules.sh
@@ -1,13 +1,23 @@
+<<<<<<< Updated upstream
 iptables -A INPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j DROP
 iptables -A OUTPUT -p tcp --sport 22 -j DROP
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+=======
+iptables -A INPUT -p tcp -m multiport --dports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 22,2222 -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 22,2222 -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
+>>>>>>> Stashed changes
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 iptables -A FORWARD -p tcp -m multiport --dports 3333,4444,5555 -j ACCEPT
-
+iptables -A INPUT -p
 
 iptables -N port-scanning
 iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
