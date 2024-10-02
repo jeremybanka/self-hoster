e8ef4b456f35aee0d46b6abda5bc9043b908e215
Moira Banka <moirabanka@gmail.com>
2024-10-01 21:53:34 -0700

    added rule to allow dns over udp

--- firewall/iptables-rules.sh
+++ firewall/iptables-rules.sh
@@ -1,5 +1,7 @@
 iptables -A INPUT -p tcp -m multiport --dports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p udp --sport 53 -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -m multiport --dports 22,2222 -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 22,2222 -j ACCEPT
