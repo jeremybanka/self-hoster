9d1c1ef721c642a579283dbe24e2a71cc29c8e61
Moira Banka <moirabanka@gmail.com>
2024-10-15 21:50:20 -0700

    updating iptables logging rules

--- firewall/iptables-rules.sh
+++ firewall/iptables-rules.sh
@@ -1,3 +1,6 @@
+iptables -A INPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "dropped packet (invalid): "
+iptables -A INPUT -i eth0 -m limit --limit 8/min -m conntrack --ctstate NEW -j LOG --log-prefix "new connection: "
+
 iptables -A INPUT -p tcp -m multiport --dports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 80,443,53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
@@ -15,11 +18,6 @@ iptables -A FORWARD -p tcp -m multiport --dports 3333,4444,5555 -j ACCEPT
 iptables -N port-scanning
 iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN
 iptables -A port-scanning -j DROP
-
-iptables -N LOGGING
-iptables -A INPUT -j LOGGING
-iptables -A LOGGING -m limit --limit 3/min -j LOGGING --log-prefix "IPTables-Dropped: " --log-level 4
-iptables -A LOGGING -j DROP
 
 iptables --policy INPUT DROP
 iptables --policy OUTPUT DROP
