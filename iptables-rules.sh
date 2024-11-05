2cb31fea8ffdbc42748ce4a8a055d3fb207ed552
Moira Banka <moirabanka@gmail.com>
2024-11-05 01:42:56 -0800

    streamlined iptables rules, disabled nonfunctional port scanning rules

--- iptables-rules.sh
+++ iptables-rules.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-iptables -A INPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "dropped packet (invalid): "
-iptables -A INPUT -i eth0 -m limit --limit 8/min -m conntrack --ctstate NEW -j LOG --log-prefix "new connection: "
+# log invalid packets
+iptables -A INPUT -m conntrack --ctstate INVALID -j LOG --log-prefix "iptables dropped packet (invalid): "
+# log new connections (max 8 per minute)
+iptables -A INPUT -i eth0 -m limit --limit 8/min -m conntrack --ctstate NEW -j LOG --log-prefix "iptables new connection: "
 
-# allow http, https, and dns (tcp) in
-iptables -A INPUT -p tcp -m multiport --dports 80,443,53 -j ACCEPT
+# allow established and related traffic in
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+# allow http, https, dns (tcp), ssh (default and alternative), and git in
+iptables -A INPUT -p tcp -m multiport --dports 80,443,53,22,2222,9418 -j ACCEPT
 # allow dns (udp) in
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
-# allow ssh on 22 and 2222 in
-iptables -A INPUT -p tcp -m multiport --dports 22,2222 -j ACCEPT
 # allow icmp in
 iptables -A INPUT -p icmp -j ACCEPT
 # allow loopback in
@@ -16,15 +18,12 @@ iptables -A INPUT -i lo -j ACCEPT
 # drop invalid packets
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 
-
-# allow http, https, and dns (tcp) out
-iptables -A OUTPUT -p tcp -m multiport --dports 80,443,53  -j ACCEPT
+# allow http, https, dns (tcp), ssh (default and alternative), and git out
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443,53,22,2222,9418  -j ACCEPT
 # allow dns (udp) out 
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 # allow new and established connections out
 iptables -A OUTPUT -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-# allow ssh on 22 and 2222 out
-iptables -A OUTPUT -p tcp -m multiport --dports 22,2222 -j ACCEPT
 # allow icmp out
 iptables -A OUTPUT -p icmp -j ACCEPT
 # allow loopback out
@@ -33,10 +32,10 @@ iptables -A OUTPUT -o lo -j ACCEPT
 # allow forwarding for flightdeck
 iptables -A FORWARD -p tcp -m multiport --dports 3333,4444,5555 -j ACCEPT
 
-# port scanning drop (changed to be slightly more permissive)
-iptables -N port-scanning
-iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 5/s --limit-burst 10 -j RETURN
-iptables -A port-scanning -j DROP
+# port scanning drop (ineffective, needs to cover other types of port scans without restricting wanted traffic)
+#iptables -N port-scanning
+#iptables -A port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 5/s --limit-burst 10 -j RETURN
+#iptables -A port-scanning -j DROP
 
 # default restrictive policy
 iptables --policy INPUT DROP
