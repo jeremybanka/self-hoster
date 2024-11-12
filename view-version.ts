69fd0e887ede878c62768d5a4124b25bc5dc5d71
Jeremy Banka <hello@jeremybanka.com>
2024-11-12 00:46:44 -0800

    ✨ add version view script

---
+++ view-version.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+
+const releaseVersion = `${process.argv.at(-1)}\n`;
+const availableVersion = (
+	await $`npm view tempest.games version`
+).stdout.toString();
+
+if (availableVersion !== releaseVersion) process.exit(1);
