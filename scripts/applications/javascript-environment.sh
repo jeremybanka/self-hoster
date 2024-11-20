bda92b4ca5fa54427afaec8e6511cb75fc0eae4f
Jeremy Banka <hello@jeremybanka.com>
2024-11-20 01:02:11 -0800

    🐛 pipe to bash

--- scripts/applications/javascript-environment.sh
+++ scripts/applications/javascript-environment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-curl -fsSL https://fnm.vercel.app/install
+curl -fsSL https://fnm.vercel.app/install | bash
 eval "$(fnm env)"
 if [ -f .node-version ]; then
   fnm use
 fi
 
-curl -fsSL https://bun.sh/install
+curl -fsSL https://bun.sh/install | bash
 bun upgrade
 global_packages="@antfu/ni"
 for pkg in $global_packages; do
