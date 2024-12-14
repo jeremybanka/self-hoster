3ffde4cc7fe21a149b268a77222c10c63966c05b
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 15:43:04 -0800

    ⬆️ postgresql-17

--- scripts/dependencies/apt-install.sh
+++ scripts/dependencies/apt-install.sh
@@ -11,10 +11,12 @@ apt install -y \
   iptables-persistent \
   nginx \
   openssh-server \
-  postgresql \
   python3-certbot-nginx \
   snort \
   stow \
   vim \
   zsh
 
+apt install -y postgresql-common
+/usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+apt install -y postgresql-17
