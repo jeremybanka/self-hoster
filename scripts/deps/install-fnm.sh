373391e2aa9eb8b31dd5dadaed2567e9b22f768d
Jeremy Banka <hello@jeremybanka.com>
2024-12-15 01:42:39 -0800

    ❓ look at .local

--- scripts/deps/install-fnm.sh
+++ scripts/deps/install-fnm.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-curl -fsSL https://fnm.vercel.app/install | bash
-ls /root/.local/share
+curl -fsSL https://fnm.vercel.app/install | zsh
+sudo ls -a /root/.local/share
 echo 'PATH=$PATH:/root/.local/share/fnm' >> ~/.zshrc
 source ~/.zshrc
 fnm use --install-if-missing
