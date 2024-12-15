afb7f08d1942a2db51b4842bb3846c2a486c9b39
Jeremy Banka <hello@jeremybanka.com>
2024-12-15 13:25:25 -0800

    🔧 same for install-fnm

--- scripts/deps/install-fnm.sh
+++ scripts/deps/install-fnm.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-curl -fsSL https://fnm.vercel.app/install | zsh
+curl -fsSL https://fnm.vercel.app/install | SHELL=zsh bash
 echo 'PATH=$PATH:$HOME/.local/share/fnm' >> ~/.zshrc
 echo 'eval "$(fnm env)"' >> ~/.zshrc
 source ~/.zshrc
