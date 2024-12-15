f74a6967e4a3d30d73d96e42c9762927e41a3796
Jeremy Banka <hello@jeremybanka.com>
2024-12-15 01:14:51 -0800

    ♻️ change bun order of operations

--- scripts/dependencies/globals.bun.ts
+++ scripts/dependencies/globals.bun.ts
@@ -8,9 +8,9 @@ await installNpmGlobals()
 
 async function installNpmGlobals() {
 	const globalPackageDir = join(homedir(), `.bun`, `install`, `global`)
-	console.log(`\x1b[32mUsing global package.json from:\x1b[0m`)
-	await $`bun pm ls --global`
 	console.log(`\x1b[32mInstalling with:\x1b[0m`)
 	await $`bun install --cwd ${globalPackageDir}`
+	console.log(`\x1b[32mUsed global package.json from:\x1b[0m`)
+	await $`bun pm ls --global`
 	console.log(`\x1b[32mDone!\x1b[0m`)
 }
