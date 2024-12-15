fc275a2002c870e00cb68ee3c1da6b8c0d4e48d0
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 18:32:49 -0800

    🐛 source .zshrc to add zplug to path

--- scripts/dependencies/install-omz.sh
+++ scripts/dependencies/install-omz.sh
@@ -2,4 +2,5 @@
 
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+source ~/.zshrc
 zplug "so-fancy/diff-so-fancy", as:command, use:bin/git-dsf
