03410267743f7475eecaacd98d93b5ee5bedbfe8
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 17:36:55 -0800

    🔧 --adopt

--- scripts/config/stow-overlay.sh
+++ scripts/config/stow-overlay.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # shellcheck disable=SC2016
-stow -t / --override='sh ./scripts/config/stow_backup.sh $stow_target' critter
-stow -t ~ --override='sh ./scripts/config/stow_backup.sh $stow_target' home
+stow --adopt -t / critter
+stow --adopt -t ~ home
