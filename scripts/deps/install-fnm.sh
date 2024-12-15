f27832db9cdfecc4df1fb46d216016c8581f1d25
Jeremy Banka <hello@jeremybanka.com>
2024-12-15 01:38:15 -0800

    ❓ what's going on in fnm setup

--- scripts/deps/install-fnm.sh
+++ scripts/deps/install-fnm.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-curl -fsSL https://fnm.vercel.app/install | zsh
+curl -fsSL https://fnm.vercel.app/install | bash
+ls /root/.local/share
 echo 'PATH=$PATH:/root/.local/share/fnm' >> ~/.zshrc
 source ~/.zshrc
 fnm use --install-if-missing
