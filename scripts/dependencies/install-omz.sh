93d8be052bbec697e7c14de0ba3be795c4224947
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 17:56:33 -0800

    🔨 install zplug

--- scripts/dependencies/install-omz.sh
+++ scripts/dependencies/install-omz.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 zplug "so-fancy/diff-so-fancy", as:command, use:bin/git-dsf
