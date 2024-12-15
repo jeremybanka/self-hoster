27c5cf6804d8f156bd2cb1457dd0288995794e03
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 20:27:06 -0800

    ❓ investigate install-bun

--- scripts/dependencies/install-bun.sh
+++ scripts/dependencies/install-bun.sh
@@ -1,12 +1,16 @@
 #!/bin/zsh
 
-set -eo pipefail
-
+echo "Installing Bun..."
 curl -fsSL https://bun.sh/install | zsh
+echo "Source ~/.zshrc..."
 source ~/.zshrc
+echo "Upgrading Bun..."
 bun upgrade
+echo "Installing global packages..."
 global_packages="@antfu/ni diff-so-fancy"
 for pkg in $global_packages; do
     bun install -g $pkg
 done
+echo "Adding ~/.bun/bin to PATH..."
 export PATH="$HOME/.bun/bin:$PATH"
+echo "Done!"
