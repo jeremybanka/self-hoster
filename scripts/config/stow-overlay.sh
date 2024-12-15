062d9b368bf5c2bad2edc8b95070d68d99fedad8
Jeremy Banka <hello@jeremybanka.com>
2024-12-15 00:55:31 -0800

    ❓ look at home

--- scripts/config/stow-overlay.sh
+++ scripts/config/stow-overlay.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+echo "adopting home as $(whoami) $HOME"
 stow --adopt -t ~ home
+echo "adopting critter as root"
 sudo stow --adopt -t / critter
+echo "adopted critter as $(whoami) $HOME"
 
 git status
 git stash
