ab64e5636a000137b768c5f043521185da8c136c
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 22:23:04 -0800

    ❓ more investigation

--- scripts/config/stow-overlay.sh
+++ scripts/config/stow-overlay.sh
@@ -4,8 +4,20 @@ stow --adopt -t / critter
 stow --adopt -t ~ home
 
 git status
-git diff
+# git diff
 git stash
 
 cat ~/.zshrc
 cat ~/.gitconfig
+
+source ~/.zshrc
+
+echo 'PATH=$PATH" >> ~/.zshrc
+
+git diff
+
+git d
+
+g diff
+
+g d
