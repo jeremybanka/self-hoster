5d831fe2823f516e81cf667d2857dd31455d20e3
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 18:41:32 -0800

    🐛 fix permissions

--- scripts/dependencies/install-omz.sh
+++ scripts/dependencies/install-omz.sh
@@ -2,5 +2,12 @@
 
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+sudo chmod g-w,o-w /usr/share/zsh
+sudo chmod g-w,o-w /usr/share/zsh/vendor-completions
+sudo chown root:root /usr/share/zsh
+sudo chown root:root /usr/share/zsh/vendor-completions
+
 source ~/.zshrc
+
 zplug "so-fancy/diff-so-fancy", as:command, use:bin/git-dsf
