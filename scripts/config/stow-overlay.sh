bdc103f1c1d6bb4be74a08a737da8807abc4a972
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 22:46:37 -0800

    🧹

--- scripts/config/stow-overlay.sh
+++ scripts/config/stow-overlay.sh
@@ -4,24 +4,6 @@ stow --adopt -t / critter
 stow --adopt -t ~ home
 
 git status
-# git diff
-
-echo "CREATED .zshrc"
-cat ~/.zshrc
-
 git stash
 
-echo "REPO .zshrc"
-cat ~/.zshrc
-
 source ~/.zshrc
-
-echo 'PATH=$PATH' >> ~/.zshrc
-
-git diff
-
-git d
-
-g diff
-
-g d
