c012f0aa4f706c682c2ffa55cdb434618dd21366
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 20:08:27 -0800

    🔧 okay, don't pipefail the omz installer

--- scripts/dependencies/install-omz.sh
+++ scripts/dependencies/install-omz.sh
@@ -1,15 +1,10 @@
 #!/bin/zsh
 
-set -eo pipefail
-
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | zsh
 
-echo "modifying zsh permissions"
 chmod g-w,o-w /usr/share/zsh
 chmod g-w,o-w /usr/share/zsh/vendor-completions
-echo "modifying zsh ownership"
 chown root:root /usr/share/zsh
 chown root:root /usr/share/zsh/vendor-completions
 
-echo "sourcing zsh"
 source ~/.zshrc
