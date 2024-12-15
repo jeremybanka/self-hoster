d9382756b0564f98472f9ae5b620c5ea21ec7b35
Jeremy Banka <hello@jeremybanka.com>
2024-12-14 20:41:30 -0800

    ♻️ simplify bun installation

--- scripts/dependencies/install-bun.sh
+++ scripts/dependencies/install-bun.sh
@@ -1,15 +1,8 @@
 #!/bin/zsh
 
-echo "Installing Bun..."
 curl -fsSL https://bun.sh/install | bash
-echo "Source ~/.zshrc..."
 source ~/.zshrc
-echo "Upgrading Bun..."
 bun upgrade
-echo "Installing global packages..."
 for pkg in @antfu/ni diff-so-fancy; do
     bun install -g $pkg
 done
-echo "Adding ~/.bun/bin to PATH..."
-export PATH="$HOME/.bun/bin:$PATH"
-echo "Done!"
