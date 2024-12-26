300dd7ecc9b954f757e45e5bf4f2d3217bdaa283
Jeremy Banka <hello@jeremybanka.com>
2024-12-25 23:02:57 -0800

    🔧 add safety to use staging by default

--- scripts/sign-certificate.sh
+++ scripts/sign-certificate.sh
@@ -2,6 +2,15 @@
 
 source ~/.zshrc
 
-sudo certbot --nginx -d tempest.games
-sudo certbot --nginx -d realtime.tempest.games
-sudo certbot --nginx -d flightdeck.tempest.games
+# Default to production
+EXTRA_ARGS="--test-cert"
+
+# If the first argument is --real, run Certbot for real
+if [ "$1" = "--real" ]; then
+  echo "Running Certbot for real..."
+  EXTRA_ARGS=""
+fi
+
+sudo certbot --nginx $EXTRA_ARGS -d tempest.games
+sudo certbot --nginx $EXTRA_ARGS -d realtime.tempest.games
+sudo certbot --nginx $EXTRA_ARGS -d flightdeck.tempest.games
